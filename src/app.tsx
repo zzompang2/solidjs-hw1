b75057a8aebd579dc216d71617d1536187161eed
zzompang2 <zzompang2@gmail.com>
2023-12-21 11:03:34 +0900

    show context-menu

--- src/app.tsx
+++ src/app.tsx
@@ -3,20 +3,31 @@ import { createSignal } from "solid-js";
 import "./app.css";
 
 export default function App() {
-  const [count, setCount] = createSignal(0);
+  let contextRef;
+
+  const openContextMenu = (e) => {
+    e.preventDefault();   // remove default context menu
+    e.stopPropagation();  // stop clicking items behind
+    console.log("right click");
+    console.log(e.target.className);
+    contextRef.style.setProperty("left", e.clientX + "px");
+    contextRef.style.setProperty("top", e.clientY + "px");
+    contextRef.style.setProperty("display", "block");
+  }
+
   return (
-    <main>
-      <h1>Hello world!</h1>
-      <button class="increment" onClick={() => setCount(count() + 1)}>
-        Clicks: {count()}
-      </button>
-      <p>
-        Visit{" "}
-        <a href="https://start.solidjs.com" target="_blank">
-          start.solidjs.com
-        </a>{" "}
-        to learn how to build SolidStart apps.
-      </p>
+    <main
+    onclick={() => contextRef.style.setProperty("display", "none")}>
+      <div
+      class="playground"
+      onContextMenu={openContextMenu}>
+        <div class="contextmenu" ref={contextRef}>
+          <div class="contextmenu-item">Add</div>
+          <div class="contextmenu-item">Delete</div>
+          <div class="contextmenu-item">Change Color</div>
+        </div>
+        <div class="box" onContextMenu={openContextMenu}></div>
+      </div>
     </main>
   );
 }
